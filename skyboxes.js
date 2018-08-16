b1fc537fabb61e942aae89ed64ee6422dcb446c5
anywhere3D <anywhere3d@gmail.com>
2018-08-16 19:37:48 +0200

    Add files via upload

---
+++ skyboxes.js
@@ -0,0 +1,76 @@
+//  skyboxes.js
+{
+
+    autumn: {
+        ext:  "jpg",
+        mime: "image/jpeg",
+        name: "autumn",
+        type: "skybox",
+        host: "https://i.imgur.com/"
+        ids: {
+            posx: "oGtEBtK",
+            negx: "KKxS9Hc",
+            posy: "a30KQuI",
+            negy: "6vSDGdo",
+            posz: "cdiEPla",
+            negz: "y4sPBum"
+        },
+        idsArray: [
+            "oGtEBtK", // "posx.jpg",
+            "KKxS9Hc", // "negx.jpg", 
+            "a30KQuI", // "posy.jpg", 
+            "6vSDGdo", // "negy.jpg", 
+            "cdiEPla", // "posz.jpg", 
+            "y4sPBum"  // "negz.jpg"
+        ]
+    },
+
+    clouds: {
+        ext:  "jpg",
+        mime: "image/jpeg",
+        name: "clouds",
+        type: "skybox",
+        host: "https://i.imgur.com/"
+        ids: {
+            posx: "Wq5hNgp",
+            negx: "miDjkfE",
+            posy: "uqV1m3t",
+            negy: "fNegTKZ",
+            posz: "BIMqAp8",
+            negz: "wJFiTpM"
+        },
+        idsArray: [
+            "Wq5hNgp", // "posx.jpg",
+            "miDjkfE", // "negx.jpg", 
+            "uqV1m3t", // "posy.jpg", 
+            "fNegTKZ", // "negy.jpg", 
+            "BIMqAp8", // "posz.jpg", 
+            "wJFiTpM"  // "negz.jpg"
+        ]
+    },
+
+    space4: {
+        ext:  "jpg",
+        mime: "image/jpeg",
+        name: "space4",
+        type: "skybox",
+        host: "https://i.imgur.com/"
+        ids: {
+            posx: "v6bjQLb",
+            negx: "lwrlr6P",
+            posy: "kKUKBJg",
+            negy: "N0oZlJR",
+            posz: "x9q8z0K",
+            negz: "HYcK7Ii"
+        },
+        idsArray: [
+            "v6bjQLb", // "posx.jpg",
+            "lwrlr6P", // "negx.jpg", 
+            "kKUKBJg", // "posy.jpg", 
+            "N0oZlJR", // "negy.jpg", 
+            "x9q8z0K", // "posz.jpg", 
+            "HYcK7Ii"  // "negz.jpg"
+        ]
+    }
+
+}
